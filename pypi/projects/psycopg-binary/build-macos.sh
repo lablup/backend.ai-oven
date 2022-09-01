d7a056689b0031c4e30580f717e81023e2784997
Joongi Kim <me@daybreaker.info>
2022-09-01 13:29:50 +0900

    Fix the output path for psycopg[binary] wheel

--- pypi/projects/psycopg-binary/build-macos.sh
+++ pypi/projects/psycopg-binary/build-macos.sh
@@ -3,6 +3,7 @@
 cleanup() {
   pyenv uninstall -f "${VENV_BUILD}"
   rm -rf "$tmppath"
+  rm -rf "$origpath/$VERSION"
   cd "$origpath"
 }
 
@@ -11,6 +12,7 @@ set -e
 origpath="$(pwd)"
 tmppath="$(mktemp -d)"
 VERSION=${VERSION:-3.1.0}
+VERSION_TAG=${VERSION_TAG:-3.1}
 PYTHON_VERSION=${PYTHON_VERSION:3.10.5}
 VENV_BUILD="tmp-psycopg-binary-build"
 
@@ -22,7 +24,7 @@ cd "$tmppath"
 pyenv local "${VENV_BUILD}"
 pip install -U pip setuptools wheel
 pip wheel -w ./wheelhouse "psycopg[c]==${VERSION}"
-cp ./wheelhouse/psycopg*.whl $origpath/$VERSION
-mv "$origpath/$VERSION/psycopg_c-3.1-cp310-cp310-macosx_12_0_arm64.whl" \
-   "$origpath/$VERSION/psycopg_binary-3.1-cp310-cp310-macosx_12_0_arm64.whl"
+cp ./wheelhouse/psycopg*.whl "$origpath/$VERSION"
+mv "$origpath/$VERSION/psycopg_c-${VERSION_TAG}-cp310-cp310-macosx_12_0_arm64.whl" \
+   "$origpath/psycopg_binary-${VERSION_TAG}-cp310-cp310-macosx_12_0_arm64.whl"
 cd "$origpath"
