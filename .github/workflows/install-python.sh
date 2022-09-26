4d9d9638196c8baf739ca5cd75a44de458bc709f
Kyujin Cho <kyujin.cho@lablup.com>
2022-09-26 20:12:58 +0900

    use github's macos action runner to build grpcio wheels

---
+++ .github/workflows/install-python.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+target_versions="3.9.13 3.10.7"
+
+for version in $target_versions; do
+    major_minor=$(echo "${version}" | awk -F. '{printf("%s.%s",$1,$2)}')
+    pkgutil --pkgs | grep "PythonFramework-${major_minor}" > /dev/null
+    if [ $? -ne 0 ]; then
+        echo "Downloading https://www.python.org/ftp/python/${version}/python-${version}-macos11.pkg"
+        curl "https://www.python.org/ftp/python/${version}/python-${version}-macos11.pkg" > "python-${version}-macos11.pkg"
+        echo "Installing python ${version} on system"
+        sudo installer -pkg "python-${version}-macos11.pkg" -target /
+    fi
+done
