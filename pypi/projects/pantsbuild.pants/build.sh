0252c11501a720c13cc2c7de3cd9d6cb35ec2758
Joongi Kim <joongi@lablup.com>
2022-12-03 17:01:49 +0000

    Add normalized version of pantsbuild.pants pkg name

---
+++ pypi/projects/pantsbuild.pants/build.sh
@@ -0,0 +1,6 @@
+# TODO: automate
+#   - Use cibuildwheel container
+#   - Install rustup
+#   - Clone pantsbuild-pants repo
+#   - Prepare Python 3.9
+#   - Run "USE_PY39=true ./build-support/bin/release.sh build-wheels" 
