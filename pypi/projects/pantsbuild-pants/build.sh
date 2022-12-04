8c9945b8acea4c35bf5ed4c773c92473068200c5
Joongi Kim <joongi@lablup.com>
2022-12-04 01:51:22 +0000

    Include git hash in the pantsbuild-pants pkg version

--- pypi/projects/pantsbuild-pants/build.sh
+++ pypi/projects/pantsbuild-pants/build.sh
@@ -2,5 +2,7 @@
 #   - Use cibuildwheel container
 #   - Install rustup
 #   - Clone pantsbuild-pants repo
+#   - Checkout "2.14.x"
+#     (this "major.minor.x" branch name pattern is recogznied by the build script to generate a release version wheel)
 #   - Prepare Python 3.9
 #   - Run "USE_PY39=true ./build-support/bin/release.sh build-wheels" 
